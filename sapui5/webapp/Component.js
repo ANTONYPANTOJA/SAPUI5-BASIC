a9f9f51f6c056f1fcf9a694df5fbddff6e468220
ANTONYPANTOJA <anthonypantojaari@gmail.com>
2022-03-24 01:03:33 +0000

    Bloque_II_Interfaz_usuario_vinculaciones_formateadores

--- sapui5/webapp/Component.js
+++ sapui5/webapp/Component.js
@@ -2,7 +2,8 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     'logaligroup/sapui5/model/Models',
-    'sap/ui/model/resource/ResourceModel'
+    'sap/ui/model/resource/ResourceModel',
+    './controller/HelloDialog'
 
     /** 
     *  @param {typeof sap.ui.core.UIComponent} UIComponent
@@ -10,7 +11,7 @@ sap.ui.define([
     *  @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
     */
 
-], function (UIComponent, Models, ResourceModel) {
+], function (UIComponent, Models, ResourceModel,HelloDialog) {
     'use strict';
     return UIComponent.extend("logaligroup.sapui5.Component", {
 
@@ -36,7 +37,18 @@ sap.ui.define([
             var i18nModel = new ResourceModel({ bundleName: "logaligroup.sapui5.i18n.i18n" });
             this.setModel(i18nModel, "i18n");
 
-        }
+            //Componente centralizado HelloDialog
+              this._helloDialog = new HelloDialog(this.getRootControl());
+        },
+
+         exit : function(){
+             this._helloDialog.destroy();
+             delete this._helloDialog;
+
+         },
+         openHelloDialog : function(){
+             this._helloDialog.open();
+         }
 
     });
 
